2407c98252125a0cfbc5fb43cd5fec2b515d506b
mounasellami <sellamimouna25@gmail.com>
2022-02-13 19:41:00 +0100

    app-classroom

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,23 @@ import About from "./Components/About/About";
 import SearchBox from './Components/SearchBox/SearchBox';
 import Watch from './Components/Watch/Watch';
 import Footer from './Components/Footer/Footer';
+import PartyEmoji from './res/PartyEmoji.png';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
  
   const isWeekend=([0,6].indexOf(new Date().getDay()) !== -1); ;// 6 = Saturday, 0 = Sunday
   // const isWeekend=true;
+  // const isWeekend=false;
 
   if (isWeekend) {
     return( 
      <div className="App-section">
         <Header/>
         <About/>
-        <h3> Enjoy is a weekend :) Keep remember your question to monday.</h3> 
+        <h3 style={{color:'red', textAlign:'center'}}> 
+          Enjoy is a weekend <img style={{height:"7%", width:"3%"}} src={PartyEmoji} alt='Party Emoji'/> Keep remember your question to monday.
+        </h3> 
         <Watch/>
         <Footer/>
      </div>
